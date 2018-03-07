519f5b15776b48897b1d30abc7b8e1e8665a5aed
Ethel C <cofresi.ethel@gmail.com>
2018-03-07 02:57:04 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+//eCofresi
+//board = ["X", "O", "X", "X", "O", "X", "O", "O", " "]
+
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
+  end
+  puts counter
+  return counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+//current_player(board)
